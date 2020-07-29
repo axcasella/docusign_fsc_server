b2a06db70022c564d4c44722b171b7f9d92933ea
Alex Casella <alex.casella@ibm.com>
2020-07-28 20:40:06 -0400

    update

--- src/config/config.ts
+++ src/config/config.ts
@@ -12,5 +12,5 @@ export const azureADURL =
 export const resource = "https://greenblocks.crm.dynamics.com/";
 export const client_id = "ae51fcee-318b-4178-9c60-627bef57f331";
 export const username = "axc@greenblocks.onmicrosoft.com";
-export const password = "MSpassword123";
+export const password = "etf67sqg@!pk";
 export const client_secret = "2q4-g3U3LE7c8nDIjS7~~.wL~a.t4ltFHq";

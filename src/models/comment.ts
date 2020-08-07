d025de78c9bb75665ef125f9f4febb65571651ac
Alex Casella <alex.casella@ibm.com>
2020-08-07 14:39:16 -0400

    added timestamp to comment

--- src/models/comment.ts
+++ src/models/comment.ts
@@ -14,6 +14,10 @@ const commentSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const commentModel = mongoose.model<Comment>("comment", commentSchema);
